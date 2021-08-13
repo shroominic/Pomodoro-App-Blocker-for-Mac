b171dd384b43878aa706a1c71747349832a8d019
Dominic <DB2002Dominic@gmail.com>
2021-08-13 18:05:02 +0200

    custom_session mode
    you can now set a session length

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from AppKit import NSWorkspace
 import subprocess
 import time
+import asyncio
+from threading import Timer
+
 
 class AppBlocker:
     def __init__(self, blacklist, whitelist):
@@ -12,25 +15,42 @@ class AppBlocker:
     def refresh(self):
         self.running_apps = [apps["NSApplicationName"] for apps in NSWorkspace.sharedWorkspace().launchedApplications()]
 
+    def read_config(self, config_path):
+        pass #TODO
+
+    def stop_blocking(self):
+        self.running = False
+        print('- app blocking stopped -')
+
     def close_app(self, app):
+        print(f'Closing: {app}')
         subprocess.call(['osascript', '-e', f'tell application "{app}" to quit'])
 
     def start_blocking(self):
+        print('- start app blocking -')
         while self.running:
-    
             self.refresh()
             time.sleep(1)
 
             for app in self.running_apps:
                 if app in blacklist:
-                    print(app)
                     self.close_app(app)
 
+    def custom_session(self, duration):
+        timer = Timer(duration, self.stop_blocking)
+        timer.start()
+        self.start_blocking()
+        timer.join()
 
+    def pomodoro(self):
+        print('Pomodoro Mode Activated - 25min Intense Focus')
+        self.custom_session(25*60)
+        
 
-blacklist = ['Brave Browser', 'Safari', 'Twitter', 'WhatsApp']
+blacklist = ['Brave Browser', 'Twitter', 'WhatsApp', 'Mail']
 whitelist = ['Notion', 'Anki', 'Fantastical', 'ToDoist']
+
 
 if __name__ == '__main__':
     appBlocker = AppBlocker(blacklist, whitelist)
-    appBlocker.start_blocking()
+    appBlocker.pomodoro()
