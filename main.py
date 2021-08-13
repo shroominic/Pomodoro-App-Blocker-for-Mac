ba6bee4af1a7f210dfe60e0e6531a61204da4d93
Dominic <DB2002Dominic@gmail.com>
2021-08-13 18:56:22 +0200

    adding support for config.json

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
 from AppKit import NSWorkspace
 import subprocess
 import time
+import json
 import asyncio
 from threading import Timer
 
 
 class AppBlocker:
-    def __init__(self, blacklist, whitelist):
-        self.blacklist = blacklist
-        self.whitelist = whitelist
+    def __init__(self, config_path, blacklist = [], whitelist = []):
+        self.config_path = config_path
         self.running_apps = []
+        self.deamonMode = True
         self.running = True
+
+        self.blacklistActiv = True
+        self.blacklist = blacklist
+        self.whitelistActiv = False
+        self.whitelist = whitelist
 
     def refresh(self):
         self.running_apps = [apps["NSApplicationName"] for apps in NSWorkspace.sharedWorkspace().launchedApplications()]
+        if self.config_path != None:
+            self.read_config()
 
-    def read_config(self, config_path):
-        pass #TODO
+    def read_config(self):
+        with open(self.config_path, 'r') as f:
+            config = json.load(f)
 
+
+        self.deamonMode = config['deamonMode']
+        self.blacklistActiv = config['blacklistActiv']
+        self.whitelistActiv = config['whitelistActiv']
+        self.blacklist = config['blacklist']
+        self.whitelist = config['whitelist']
+        
     def stop_blocking(self):
         self.running = False
         print('- app blocking stopped -')
@@ -33,8 +49,12 @@ class AppBlocker:
             time.sleep(1)
 
             for app in self.running_apps:
-                if app in blacklist:
-                    self.close_app(app)
+                if self.blacklistActiv:
+                    if app in self.blacklist:
+                        self.close_app(app)
+                elif self.whitelistActiv:
+                    if not app in self.whitelist:
+                        self.close_app(app)
 
     def custom_session(self, duration):
         timer = Timer(duration, self.stop_blocking)
@@ -45,12 +65,11 @@ class AppBlocker:
     def pomodoro(self):
         print('Pomodoro Mode Activated - 25min Intense Focus')
         self.custom_session(25*60)
-        
-
-blacklist = ['Brave Browser', 'Twitter', 'WhatsApp', 'Mail']
-whitelist = ['Notion', 'Anki', 'Fantastical', 'ToDoist']
 
 
-if __name__ == '__main__':
-    appBlocker = AppBlocker(blacklist, whitelist)
+if __name__ == "__main__":
+
+    config_path = 'config.json'
+    appBlocker = AppBlocker(config_path)
+
     appBlocker.pomodoro()
